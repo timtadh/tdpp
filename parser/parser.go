1e89036e71505e6c3523c32fe28c9ef09a4c873a
Tim Henderson <tim.tadh@gmail.com>
2010-04-02 17:46:45 -0400

    switch tokens []int to a chan so the parser can operate on a stream
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- parser/parser.go
+++ parser/parser.go
@@ -3,33 +3,32 @@ package parser
 import "log"
 import . "stack"
 
-func (self *Grammar) Parse(M Table, tokens []int) (chan int, chan bool) {
+func (self *Grammar) Parse(M Table, tokens chan int) (chan int, chan bool) {
     yield := make(chan int)
     ack := make(chan bool)
     go func() {
-        next := func(i int) (token, index int) {
-            if i == len(tokens) {
-                return end, i+1
+        next := func() int {
+            r := <-tokens
+            if closed(tokens) {
+                return end
             }
-            return tokens[i], i+1
+            return r
         }
-        var a int
         stack := NewStack()
-        index := 0
         stack.Push(end)
         stack.Push(self.ORDER[0])
         X := stack.Peek()
-        a, index = next(index)
+        a := next()
         for X != end {
             if X == a {
                 yield <- a
                 <-ack
                 stack.Pop()
-                a, index = next(index)
+                a = next()
             } else if _, has := self.T[X]; has {
                 log.Exit("error 1")
             } else if M[X][a] == -1 {
-                log.Exit("error 2")
+                log.Exit("error 2", self.ALL[X], self.ALL[a])
             } else {
                 yield <- X
                 <-ack
